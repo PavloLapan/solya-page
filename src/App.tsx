14fb72a15bec90f92bda353a2e5f88ba0216ea7c
Pavlo.Lapan <Pavlo.Lapan@microfocus.com>
2023-03-12 03:31:52 +0200

    c2

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Main from "./components/main/main";
 import Header from "./components/header/header";
 import Footer from "./components/footer/footer";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './app.css'
+import './App.css'
 import './app.scss'
 
 function App() {
