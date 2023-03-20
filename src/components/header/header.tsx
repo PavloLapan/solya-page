40a776d4463166822629a17e30a2303d58f8c869
Pavlo.Lapan <Pavlo.Lapan@microfocus.com>
2023-03-20 23:27:05 +0200

    replace with lib

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useCallback} from "react";
 import Button from 'react-bootstrap/Button';
 import './header.scss'
 // @ts-ignore
@@ -7,11 +7,102 @@ import {faHouseLaptop} from "@fortawesome/free-solid-svg-icons";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faB } from '@fortawesome/free-solid-svg-icons'
 import {faLinkedin, faBehance, faTelegramPlane, faInstagram} from "@fortawesome/free-brands-svg-icons";
+import Particles from "react-particles";
+import type { Container, Engine } from "tsparticles-engine";
+
+import json from './particles.json'
+
+import { loadFull } from "tsparticles";
 
 const Header = () => {
+
+    const particlesInit = useCallback(async (engine: Engine) => {
+        console.log(engine);
+
+        // you can initialize the tsParticles instance (engine) here, adding custom shapes or presets
+        // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+        // starting from v2 you can add only the features you need reducing the bundle size
+        await loadFull(engine);
+    }, []);
+
+    const particlesLoaded = useCallback(async (container: Container | undefined) => {
+        await console.log(container);
+    }, []);
+
     return (
         <div id='particles-js' className="container-fluid content-middle header-container">
-
+            <Particles options={{
+                background: {
+                    color: {
+                        value: "#111111",
+                    },
+                },
+                fpsLimit: 120,
+                interactivity: {
+                    events: {
+                        onClick: {
+                            enable: true,
+                            mode: "push",
+                        },
+                        onHover: {
+                            enable: true,
+                            mode: "repulse",
+                        },
+                        resize: true,
+                    },
+                    modes: {
+                        push: {
+                            quantity: 4,
+                        },
+                        repulse: {
+                            distance: 100,
+                            duration: 0.4,
+                        },
+                    },
+                },
+                particles: {
+                    color: {
+                        value: "#ffffff",
+                    },
+                    links: {
+                        color: "#ffffff",
+                        distance: 150,
+                        enable: true,
+                        opacity: 0.5,
+                        width: 1,
+                    },
+                    collisions: {
+                        enable: true,
+                    },
+                    move: {
+                        direction: "none",
+                        enable: true,
+                        outModes: {
+                            default: "bounce",
+                        },
+                        random: false,
+                        speed: 2,
+                        straight: false,
+                    },
+                    number: {
+                        density: {
+                            enable: true,
+                            area: 800,
+                        },
+                        value: 80,
+                    },
+                    opacity: {
+                        value: 0.5,
+                    },
+                    shape: {
+                        type: "circle",
+                    },
+                    size: {
+                        value: { min: 1, max: 5 },
+                    },
+                },
+                detectRetina: true,
+            }} init={particlesInit}/>
             <div className='some-links'>
                 <a href="#part3">Етапи роботи</a>
 
